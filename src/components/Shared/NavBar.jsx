422ead2a6d58507b70dec911d247dec8bce49d5d
adhithyaramesh <rachu7875@gmail.com>
2025-07-11 14:44:54 +0530

    updated navbar

--- src/components/Shared/NavBar.jsx
+++ src/components/Shared/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Tab, Tooltip, Typography } from '@mui/material';
-import { GetRequest } from '../../api/Config';
+import { GetRequest } from '../../api/config';
 import { HeaderContainer, StyledTabs, TooltipHover } from '../../styles/NavBar';
 import { useNavigate } from "react-router-dom";
 
